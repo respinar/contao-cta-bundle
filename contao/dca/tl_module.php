de4b23f330d085c38a55d47e8592c286b599b3a5
Hamid Peywasti <hamid@respinar.com>
2025-03-21 19:01:06 +0330

    add ctaIsVisible field

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 $GLOBALS["TL_DCA"]["tl_module"]["palettes"]["calltoaction"] = '
     {title_legend},name,type;
-    {whatsapp_legend},ctaText,ctaUrl,ctaTitle;
+    {whatsapp_legend},ctaText,ctaIsVisible,ctaUrl,ctaTitle;
     {template_legend:hide},cssID,customTpl;
     {protected_legend:hide},protected;
 ';
+
+// Define fields
+$GLOBALS["TL_DCA"]["tl_module"]["fields"]["ctaIsVisible"] = [
+    "exclude" => true,
+    "inputType" => "checkbox",
+    "eval" => ["tl_class" => "w50 m12"],
+    "sql" => ['type' => 'boolean', 'default' => true],
+];
 
 $GLOBALS["TL_DCA"]["tl_module"]["fields"]["ctaTitle"] = [
     "inputType" => "text",
