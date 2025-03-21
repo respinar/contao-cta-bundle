c579c16b17692a7d3e9c457b4ae097fe1c416286
Hamid Peywasti <hamid@respinar.com>
2025-03-21 19:03:03 +0330

    check visibility by isCtaVisible()

--- src/Controller/FrontendModule/CtaController.php
+++ src/Controller/FrontendModule/CtaController.php
@@ -20,10 +20,7 @@ class CtaController extends AbstractFrontendModuleController
         // Check if the current page has CTA fields set
         $page = $this->getPageModel();
 
-        // Get the root page
-        $rootPage = PageModel::findById($page->rootId);
-        if ($rootPage === null) {
-            // Log an error or handle gracefully; for now, assume CTA is disabled
+        if (!$this->isCtaVisible($page, $model)) {
             return new Response();
         }
 
